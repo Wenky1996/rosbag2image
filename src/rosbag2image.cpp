d3bda79480821db42c099835b47ea40105e97c14
wenky <wenkyjong1996@gmail.com>
2021-08-03 12:19:36 +0800

    Initial commit

---
+++ src/rosbag2image.cpp
@@ -0,0 +1,20 @@
+/***************************************
+ @Time : 2021/8/3 上午11:06
+ @Author : WenkyJong
+ @Site : MianYang SWUST
+ @File : rosbag2image.cpp
+ @Contact: wenkyjong1996@gmail.com
+ @desc:
+*******/
+
+
+#include <ros/ros.h>
+#include <iostream>
+
+using namespace std;
+int main(int argc , char **argv){
+
+}
+
+
+
